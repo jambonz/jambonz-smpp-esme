ed793e2041852b87c363ac2207500594a1bf58c5
Dave Horton <daveh@beachdognet.com>
2021-05-31 10:13:48 -0400

    initial testing

--- app.js
+++ app.js
@@ -6,21 +6,10 @@ const opts = Object.assign({
 });
 const logger = require('pino')(opts);
 const port = process.env.HTTP_PORT || 3020;
-const {
-  lookupSmppGateways
-} = require('@jambonz/db-helpers')({
-  host: process.env.JAMBONES_MYSQL_HOST,
-  user: process.env.JAMBONES_MYSQL_USER,
-  port: process.env.JAMBONES_MYSQL_PORT || 3306,
-  password: process.env.JAMBONES_MYSQL_PASSWORD,
-  database: process.env.JAMBONES_MYSQL_DATABASE,
-  connectionLimit: process.env.JAMBONES_MYSQL_CONNECTION_LIMIT || 10
-}, logger);
 
 app.locals = {
   ...app.locals,
   logger,
-  lookupSmppGateways
 };
 const routes = require('./lib/routes');
 
